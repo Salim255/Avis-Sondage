303a10682afb018e1bd6dd487ae64490dfdfe4fe
Salim <salim.hassan.welleat@gmail.com>
2022-10-26 13:16:04 +0200

    Add: reviewSlice and store the reviews in the store

---
+++ avis-sondage/src/features/review/reviewSlice.js
@@ -0,0 +1,92 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
+import customFetch from "../../components/axios";
+import { toast } from "react-toastify";
+
+const initialState = {
+  reviewItems: [],
+
+  isLoading: true,
+};
+
+export const getAllReviews = createAsyncThunk(
+  "review/getReviews",
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().user.user.token;
+      const resp = await customFetch.get("/opinions", {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response);
+    }
+  }
+);
+
+const reviewSlice = createSlice({
+  name: "reviews",
+  initialState,
+  reducers: {
+    /*   calculateComments: (state) => {
+      let totalComments = 0;
+      state.reviewItems.forEach((item) => {
+        if (item.review !== "") {
+          totalComments++;
+        }
+      });
+      state.totalComments = totalComments;
+    },
+
+    calculatAvgRatting: (state) => {
+      let avgRatting = 0;
+      let sumAllRatting = 0;
+      let nmRatting = 0;
+      state.reviewItems.forEach((item) => {
+        sumAllRatting = sumAllRatting + item.ratting;
+        nmRatting++;
+      });
+      avgRatting = ((sumAllRatting * 5) / (nmRatting * 5)).toFixed(1);
+      state.avgRatting = avgRatting;
+    },
+
+    calculateCallPercentage: (state) => {
+      let callBack = 0;
+      let rattingnm = 0;
+      let callPercentage = 0;
+
+      state.reviewItems.forEach((item) => {
+        rattingnm++;
+        if (item.call) {
+          callBack++;
+        }
+      });
+      callPercentage = ((100 * callBack) / rattingnm).toFixed(0);
+      state.callPercentage = callPercentage;
+    }, */
+  },
+  extraReducers: {
+    [getAllReviews.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getAllReviews.fulfilled]: (state, { payload }) => {
+      state.isLoading = false;
+      state.reviewItems = payload;
+    },
+    [getAllReviews.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+  },
+});
+
+export const {
+  calculateComments,
+  calculatAvgRatting,
+  calculateCallPercentage,
+} = reviewSlice.actions;
+
+export default reviewSlice.reducer;
