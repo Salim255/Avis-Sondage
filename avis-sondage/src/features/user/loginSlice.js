534e9785a4a376d3c0ff1699a0aed5b25c9dba7c
Salim <salim.hassan.welleat@gmail.com>
2022-10-26 11:53:50 +0200

    Add: user info to the localStorage and get the use info from the localSorage

--- avis-sondage/src/features/user/loginSlice.js
+++ avis-sondage/src/features/user/loginSlice.js
@@ -1,21 +1,48 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
+import customFetch from "../../components/axios";
+import {
+  addUserToLocalStorage,
+  getUserFromLocalStorage,
+} from "../../components/localStorage";
 
 const initialState = {
   isLoading: false,
-  user: null,
+  user: getUserFromLocalStorage(),
 };
-
-const userSlice = createSlice({
-  name: "user",
-  initialState,
-});
 
 export const loginUser = createAsyncThunk(
   "user/loginUser",
   async (user, thunkAPI) => {
-    console.log(`Login User: ${JSON.stringify(user)}`);
+    try {
+      const resp = await customFetch.post("/users/login", user);
+
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response);
+    }
   }
 );
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  extraReducers: {
+    [loginUser.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [loginUser.fulfilled]: (state, { payload }) => {
+      const user = payload;
+      state.isLoading = false;
+      state.user = user;
+      addUserToLocalStorage(user);
+      toast.success(`Welcome Back ${user.lastname} `);
+    },
+    [loginUser.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error("Invalid credentials");
+    },
+  },
+});
 
 export default userSlice.reducer;
