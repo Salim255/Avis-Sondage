a5630a8fe13d8f71c798ffe4a96a389a6698d609
Salim <salim.hassan.welleat@gmail.com>
2022-10-28 10:48:03 +0200

    Add: Fomr input component

--- avis-sondage/src/components/FormRow.js
+++ avis-sondage/src/components/FormRow.js
@@ -2,10 +2,9 @@ const FormRow = ({ type, name, value, handleChange, labelText }) => {
   return (
     <div className="form-row">
       <label htmlFor={name} className="form-label">
-        {labelText || name}
+        {labelText}
       </label>
       <input
-        id={name}
         type={type}
         name={name}
         value={value}
