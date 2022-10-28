e159b8a94f87f2d345f30b1eb2d6384aa4390d6e
Salim <salim.hassan.welleat@gmail.com>
2022-10-28 11:14:53 +0200

    Add: setup Control our create input

--- avis-sondage/src/components/CreateSurvey.js
+++ avis-sondage/src/components/CreateSurvey.js
@@ -1,73 +1,97 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { toast } from "react-toastify";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import FormRow from "./FormRow";
-import { handleChange, clearValues } from "../features/survey/surveySlice";
+import { DatePicker } from "antd";
+import moment from "moment";
 
+const initialState = {
+  title: "",
+  context: "",
+};
 export default function CreateSurvey() {
-  const dispatch = useDispatch();
-  const { isLoading, isEditing, editSurvey, position } = useSelector(
-    (store) => store.survey
-  );
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [values, setValues] = useState(initialState);
 
-    if (!position) {
-      toast.error("Please fill out all fields");
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { title, context } = values;
+    if (!title || !context || !startDate || !endDate) {
+      console.log("Please fill out all fields");
     }
   };
 
-  const handleSurveyInput = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    dispatch(handleChange({ name, value }));
-  };
-
   return (
-    <div>
-      <form className="form">
-        <h3>{isEditing ? "edit survey" : "add survey"}</h3>
-        <div className="form-center">
-          <FormRow
-            type="text"
-            name="position"
-            value={position}
-            labelText="survey description"
-            handleChange={handleSurveyInput}
-          />
-          <FormRow
-            type="text"
-            name="position"
-            value={position}
-            labelText="survey description"
-            handleChange={handleSurveyInput}
-          />
-          <FormRow
-            type="text"
-            name="position"
-            value={position}
-            labelText="survey description"
-            handleChange={handleSurveyInput}
-          />
-          <div className="btn-container">
-            <button
-              type="button"
-              className="btn btn-blak cealr-btn"
-              onClick={() => dispatch(clearValues())}
-            >
-              clear
-            </button>
-            <button
-              type="submit"
-              className="btn btn-blak submit-btn"
-              onClick={() => handleSubmit}
-              disabled={isLoading}
-            >
-              submit
-            </button>
+    <section className="create-survey">
+      <h1 className="create-survey__header">CRÉER SONDAGE</h1>
+      <div className="create-survey__container">
+        <div className="subdate__date form__date">
+          <div>
+            Commence
+            <DatePicker
+              className="subdate__date--input "
+              onChange={(date) => {
+                const value1 = moment(date).format("YYYY-MM-DD");
+
+                setStartDate(value1);
+              }}
+            />
+          </div>
+
+          <p className="subdate__date--seperator">AU</p>
+          <div>
+            Termine
+            <DatePicker
+              onChange={(date) => {
+                const value2 = moment(date).format("YYYY-MM-DD");
+                setEndDate(value2);
+              }}
+              className="datePicker date"
+            />
           </div>
         </div>
-      </form>
-    </div>
+
+        <form className="form " onSubmit={onSubmit}>
+          <div className="form__center">
+            <div>
+              {/* Survey Title */}
+              <FormRow
+                type="text"
+                name="title"
+                value={values.title}
+                handleChange={handleChange}
+                labelText="Titre Du sondage"
+              />
+              {/* Survey Context */}
+              <FormRow
+                type="text"
+                name="context"
+                value={values.context}
+                handleChange={handleChange}
+                labelText="Contexte"
+              />
+            </div>
+
+            <div className="btn-container">
+              <div>
+                <button type="submit" className="btn btn-blak submit-btn">
+                  submit
+                </button>
+
+                <Link to="/sondage" className="btn btn-blak submit-btn">
+                  Cancel
+                </Link>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 }
